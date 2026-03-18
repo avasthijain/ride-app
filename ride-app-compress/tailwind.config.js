f79a477e7ecb26a50e979b3ae4371b488f809de6
avasthijain <avasthi911jain@gmail.com>
2026-03-18 17:51:38 +0530

    Update tailwind.config.js

--- ride-app-compress/tailwind.config.js
+++ ride-app-compress/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkmode: "class",
     content: [
       "./app/**/*.{js,ts,jsx,tsx}",      
       "./components/**/*.{js,ts,jsx,tsx}",
